049eed92040ad49847e29c86fcb25adca4c0fac5
manju <manju@gmail.com>
2022-12-01 14:11:50 +0530

    add

--- src/components/watchlist/removefromwatchlist.js
+++ src/components/watchlist/removefromwatchlist.js
@@ -8,7 +8,8 @@ export const removeFromWatchlist = (id,e) => {
       const newList = previousList.filter((item) => item != id);
       console.log("newlist is>>", newList.toString());
       localStorage.setItem("watchlist", newList.toString());
-      window.location.reload();
+      //window.location.reload();
+      window.top.location=window.top.location
       // e.preventDefault();
     }
   };
